5f31d1dc8083aba120914e97c23d2e8f047d6993
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-10 04:01:27 +0530

    Runtime validation, Server configuration checks added

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,11 +1,25 @@
 import { Context } from "hono"
+import {registerValidation, signInValidation ,  } from "../utilities/validation/zodValidation";
+import { createPrismaClient } from "../config/database";
 
 const register = async(c: Context) => {
+    const body = await c.req.json(); 
+
     try {
         const { DATABASE_URL, JWT_SECRET } = c.env;
         if (!DATABASE_URL || !JWT_SECRET) {
             return c.json({ message: "Server configuration error" }, 500);
         }
+
+        const db = createPrismaClient(DATABASE_URL);
+
+        const userData = registerValidation.safeParse(body);
+        if(!userData.success){
+            return c.json({
+                message: "Invalid Input",
+                error: userData.error.flatten()
+            })
+        };
         
     } catch (error) {
         return c.json({
@@ -16,11 +30,23 @@ const register = async(c: Context) => {
 }
 
 const signin = async(c: Context) => {
+    const body = await c.req.json(); 
+
     try {
         const { DATABASE_URL, JWT_SECRET } = c.env;
         if (!DATABASE_URL || !JWT_SECRET) {
             return c.json({ message: "Server configuration error" }, 500);
         }
+
+        const db = createPrismaClient(DATABASE_URL); 
+
+        const userData = signInValidation.safeParse(body); 
+        if(!userData.success){
+            return c.json({
+                message : "Invalid Input data",
+                error: userData.error.flatten()
+            })
+        };
         
     } catch (error) {
         return c.json({
