3206509d92168df921b480f6715cb40cd7da2f99
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-10 22:56:49 +0530

    Added checkAuth route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
-import { register, signin } from "../controllers/auth.controllers";
+import { checkAuth, register, signin } from "../controllers/auth.controllers";
 import { Binding } from "../types/envInterface";
 
 const authRouter = new Hono<{ Bindings: Binding }>(); 
 
+authRouter.get("/", checkAuth);
 authRouter.post("/register", register);
 authRouter.post("/signin", signin);
+
 
 export default authRouter; 
