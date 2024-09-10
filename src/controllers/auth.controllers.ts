bb349ced65ba64e499774e37c9cd4578ffc30ef4
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-10 22:25:58 +0530

    User register and sign-in controllers added

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -1,11 +1,24 @@
 import { Context } from "hono"
-import {registerValidation, signInValidation ,  } from "../utilities/validation/zodValidation";
+import { registerValidation, signInValidation } from "../utilities/validation/zodValidation";
 import { createPrismaClient } from "../config/database";
+import bcryptjs from 'bcryptjs'
+import { sign } from "hono/jwt";
+import { setCookie } from "hono/cookie";
 
 const register = async(c: Context) => {
-    const body = await c.req.json(); 
-
     try {
+        const body = await c.req.json(); 
+
+        const userData = registerValidation.safeParse(body);
+        if(!userData.success){
+            return c.json({
+                message: "Invalid Input",
+                error: userData.error.flatten()
+            },400)
+        };
+
+        const { name, email, password } = userData.data 
+
         const { DATABASE_URL, JWT_SECRET } = c.env;
         if (!DATABASE_URL || !JWT_SECRET) {
             return c.json({ message: "Server configuration error" }, 500);
@@ -13,14 +26,35 @@ const register = async(c: Context) => {
 
         const db = createPrismaClient(DATABASE_URL);
 
-        const userData = registerValidation.safeParse(body);
-        if(!userData.success){
-            return c.json({
-                message: "Invalid Input",
-                error: userData.error.flatten()
-            })
-        };
+        const isExists = await db.user.findUnique({ where: { email }}); 
+
+        if(isExists){
+            return c.json({ message : "User already exists" } ,409); 
+        }; 
+
+        const hashedPassword = await bcryptjs.hash(password, 10); 
         
+        const user = await db.user.create({
+            data: {
+                name: name,
+                email: email, 
+                password: hashedPassword
+            },
+            select: {
+                name: true,
+                email: true
+            }
+        }); 
+
+        if(!user){
+            return c.json({ message: "Failed to create user" }, 500); 
+        }
+
+        return c.json({
+            message: "User registerd successfully",
+            data: user
+        }, 201);
+
     } catch (error) {
         return c.json({
             message: "Failed to register",
@@ -30,9 +64,19 @@ const register = async(c: Context) => {
 }
 
 const signin = async(c: Context) => {
-    const body = await c.req.json(); 
-
     try {
+        const body = await c.req.json(); 
+
+        const userData = signInValidation.safeParse(body); 
+        if(!userData.success){
+            return c.json({
+                message : "Invalid Input data",
+                error: userData.error.flatten()
+            }, 400);
+        };
+
+        const { email, password } = userData.data
+
         const { DATABASE_URL, JWT_SECRET } = c.env;
         if (!DATABASE_URL || !JWT_SECRET) {
             return c.json({ message: "Server configuration error" }, 500);
@@ -40,13 +84,43 @@ const signin = async(c: Context) => {
 
         const db = createPrismaClient(DATABASE_URL); 
 
-        const userData = signInValidation.safeParse(body); 
-        if(!userData.success){
-            return c.json({
-                message : "Invalid Input data",
-                error: userData.error.flatten()
-            })
+        const user = await db.user.findUnique({
+            where: { email }
+        }); 
+
+        if(!user){
+            return c.json({ message: "No user found with this email" }, 404);
+        }; 
+
+        const isPasswordCorrect = await bcryptjs.compare(password, user.password); 
+        if(!isPasswordCorrect){
+            return c.json({ message: "Password is incorrect" }, 401); 
+        }; 
+
+        const oneMonthInSeconds : number = 30 * 24 * 60 * 60;
+        const currentTime : number = Math.floor(Date.now() / 1000);
+        const tokenExp : number =  currentTime + oneMonthInSeconds; 
+
+        const token = await sign({ userId : user.id, exp: tokenExp }, JWT_SECRET); 
+        if(!token){
+            c.json({ message: "Failed to create token" }, 500);
         };
+
+        setCookie(c, "token", token, {
+            secure: true, 
+            httpOnly: true, 
+            sameSite: 'none',
+            expires:  new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+        }); 
+
+        return c.json({
+            message: "User signin successfully",
+            data : {
+                id: user.id,
+                email: user.email,
+                token: token
+            }
+        }, 200);
         
     } catch (error) {
         return c.json({
