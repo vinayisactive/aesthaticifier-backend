780a55764b98ca13bbd18d2e56ab271cac6ab34c
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-10 23:07:09 +0530

    authMiddleware Added

--- src/middlewares/auth.middlewares.ts
+++ src/middlewares/auth.middlewares.ts
@@ -1,8 +1,32 @@
 import { Context, Next } from "hono";
+import { getCookie } from "hono/cookie";
+import { verify } from "hono/jwt";
 
 const authMiddleware = async(c: Context, next: Next) => {
     try {
+        const token = getCookie(c, 'token')
         
+        const {JWT_SECRET } = c.env;
+        if (!JWT_SECRET) {
+            return c.json({ message: "Server configuration error" }, 500);
+        }
+
+        if (!token) {
+            return c.json({
+                authenticated: false
+            }, 401);
+        }
+
+        const currentTime : number = Math.floor(Date.now() / 1000);
+
+        const decodedToken = await verify(token, JWT_SECRET); 
+        if (decodedToken.exp && decodedToken.exp < currentTime) {
+            return c.json({ message: "Token has expired"}, 401);
+        }; 
+
+        c.set('userId', decodedToken.userId); 
+        await next(); 
+
     } catch (error) {
         return c.json({
             message: "Invalid token or authentication error",
