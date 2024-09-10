594e9cdcf74c2ce93c97d2948d44ff43214073cb
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-10 23:07:44 +0530

    auth middleware added in routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
 import { checkAuth, register, signin } from "../controllers/auth.controllers";
 import { Binding } from "../types/envInterface";
+import authMiddleware from "../middlewares/auth.middlewares";
 
 const authRouter = new Hono<{ Bindings: Binding }>(); 
 
-authRouter.get("/", checkAuth);
+authRouter.get("/",authMiddleware, checkAuth);
 authRouter.post("/register", register);
 authRouter.post("/signin", signin);
 
