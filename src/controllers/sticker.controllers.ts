74fce9cda31cb4d3c7bbc4c22033fee87c59b405
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-11 16:30:08 +0530

    Controller added to delete sticker (from cloudinary and database)

--- src/controllers/sticker.controllers.ts
+++ src/controllers/sticker.controllers.ts
@@ -1,5 +1,6 @@
 import { Context } from "hono";
 import { createPrismaClient } from "../config/database";
+import generateCloudinarySignature from "../utilities/cloudinary/generateCloudinarySignature";
 
 
 const addSticker = async(c: Context) => {
@@ -43,7 +44,7 @@ const addSticker = async(c: Context) => {
            });
 
           const result: { secure_url: string, public_id: string } = await response.json();
-           
+        
           if(!result.secure_url){
             return c.json({
                 success: false,
@@ -84,26 +85,76 @@ const addSticker = async(c: Context) => {
 
 const deleteSticker = async(c: Context) =>{
     try {
-        const { DATABASE_URL, JWT_SECRET } = c.env;
-        if (!DATABASE_URL || !JWT_SECRET) {
-            return c.json({ message: "Server configuration error" }, 500);
-        }
-        
-    } catch (error) {
-        return c.json({
-            message: "Failed to register",
-            error : error instanceof Error ? error.message : "Internal server error"
-        },500)
-    }
-}; 
+        const public_id = c.req.param('public_id');
+        if(!public_id){
+            return c.json({
+                success: false, 
+                message: "public_id is required"
+            }, 400);
+        }; 
 
-const getStickers = async(c: Context) => {
-    try {
-        const { DATABASE_URL, JWT_SECRET } = c.env;
-        if (!DATABASE_URL || !JWT_SECRET) {
-            return c.json({ message: "Server configuration error" }, 500);
+        const sticker_id = c.req.param('sticker_id');
+        if(!sticker_id){
+            return c.json({
+                success: false, 
+                message: "sticker_id is required"
+            }, 400);
+        }; 
+
+        const { DATABASE_URL, CLOUDINARY_DELETE_URL, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } = c.env;
+        if (!DATABASE_URL || !CLOUDINARY_DELETE_URL || !CLOUDINARY_API_KEY || !CLOUDINARY_API_SECRET ) {
+            return c.json({ 
+                success: false,    
+                message: "Server configuration error" 
+            }, 500);
         }
-        
+
+        const timestamp = Math.floor(Date.now() / 1000).toString();
+        const signature = await generateCloudinarySignature(public_id, timestamp, CLOUDINARY_API_SECRET);
+
+        const formData = new FormData();
+        formData.append('public_id', public_id);
+        formData.append('timestamp', timestamp.toString());
+        formData.append('api_key', CLOUDINARY_API_KEY);
+        formData.append('signature', signature);
+
+
+        const response = await fetch(CLOUDINARY_DELETE_URL, {
+            method: 'POST',
+            body: formData
+        });
+
+        const result: any = await response.json();
+
+        if (result.result !== "ok") {
+            return c.json({
+                success: false, 
+                message: "Failed to delete sticker (from Cloudinary)",
+                error: result.error ? result.error.message : "Unknown error"
+            }, 500);
+        }
+
+        const db = createPrismaClient(DATABASE_URL);
+
+        const sticker = await db.stickers.delete({
+            where: {
+                id: sticker_id
+            }
+        }); 
+
+        if(!sticker){
+            return c.json({
+                success: false, 
+                message: "Failed to delete sticker"
+            }, 500);
+        }
+
+        return c.json({
+            success: true,
+            message: "Sticker deleted successfully",
+            data: sticker
+        },200)
+
     } catch (error) {
         return c.json({
             message: "Failed to register",
@@ -114,6 +165,5 @@ const getStickers = async(c: Context) => {
 
 export {
     addSticker, 
-    deleteSticker, 
-    getStickers
+    deleteSticker
 }; 
