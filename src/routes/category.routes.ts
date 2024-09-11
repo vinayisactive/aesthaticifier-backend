8dd228c490d0949c378f7107d74dd51c6388d731
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-11 20:20:11 +0530

    Routes for update and delete category added

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,12 +1,13 @@
 import { Hono } from "hono";
-import { createCategory, getCategories, categoryWithStickers } from "../controllers/category.controllers";
+import { createCategory, getCategories, categoryWithStickers, updateCategory, deleteCategory } from "../controllers/category.controllers";
 import { authMiddleware, adminMiddleware } from "../middlewares/index";
 
 const categoryRouter = new Hono(); 
+
 categoryRouter.get("/", getCategories)
 categoryRouter.get("/stickers", authMiddleware, categoryWithStickers);
 categoryRouter.post("/", authMiddleware, adminMiddleware, createCategory);
-categoryRouter.patch("/")
-categoryRouter.delete("/");
+categoryRouter.patch("/:category_id", authMiddleware, adminMiddleware, updateCategory)
+categoryRouter.delete("/:category_id", authMiddleware, adminMiddleware, deleteCategory);
 
 export default categoryRouter; 
