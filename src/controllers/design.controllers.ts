e4c70529a4dc7286161ef28088c12c06c318fa25
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-12 19:23:18 +0530

    Added controllers body for design and design assets

---
+++ src/controllers/design.controllers.ts
@@ -0,0 +1,68 @@
+import { Context } from "hono";
+import { createPrismaClient } from "../config/database";
+
+const createDesign = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}; 
+
+const updateDesign = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const getDesigns = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const createAsset = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const getAssets = async(c:Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const updateAsset = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const deleteAsset = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export {
+    createDesign,
+    updateDesign,
+    getDesigns,
+    createAsset,
+    getAssets,
+    updateAsset,
+    deleteAsset
+}
