abc33303b571e33f3a7215aa251dab5577b3fb1f
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-09-12 20:05:04 +0530

    Controllers body for user added

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,62 @@
+import { Context } from "hono";
+
+const userDetails = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
+
+const updateUser = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}; 
+
+const userAddress = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}; 
+
+const addUserAddress = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}; 
+
+const updateUserAddress = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}; 
+
+const deleteUserAddress = async(c: Context) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}; 
+
+
+export {
+    userDetails, 
+    updateUser, 
+    addUserAddress,
+    userAddress,
+    updateUserAddress,
+    deleteUserAddress
+}
+
+
+
